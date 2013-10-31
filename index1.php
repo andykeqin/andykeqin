9211cfba6c1a72ac6ee38a0e0ca7e23182243439
andykeqin <keqin.feng.12@ucl.ac.uk>
2013-10-31 19:01:17 +0000

    Update index1.php

--- index1.php
+++ index1.php
@@ -20,7 +20,7 @@
 <p>Fill in the name which you want to search, then click <strong>Search</strong> to list it.</p>
 <form method="post" action="index1.php" enctype="multipart/form-data" >
       Name <input type="text" name="name" id="name"/></br>
-      <input type="search" name="search" value="Search" />
+      <input type="submit" name="search" value="Search" />
 </form>
 <?php
     // DB connection info
@@ -39,30 +39,9 @@
         die(var_dump($e));
     }
         
-    // Insert registration info
-    if(!empty($_POST)) {
-    try {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $date = date("Y-m-d");
-        $company = $_POST['company'];
-        // Insert data
-        $sql_insert = "INSERT INTO registration_tbl0 (name, email, date, company)
-VALUES (?,?,?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $date);
-        $stmt->bindValue(4, $company);
-        $stmt->execute();
-    }
-    catch(Exception $e) {
-        die(var_dump($e));
-    }
-    echo "<h3>Your're registered!</h3>";
-    }
-    // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl0";
+    // Search
+
+    $sql_select = "SELECT name, email, date, company FROM registration_tbl0";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
